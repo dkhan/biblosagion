7db88b8721adf3edeeee5eb8d62cff877b7c1903
Dennis Khan <khandennis@gmail.com>
2019-03-31 00:00:20 -0400

    typo

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -2,6 +2,6 @@ class Feature < ApplicationRecord
   include Discard::Model
   default_scope -> { kept }
 
-  belongs_to :text, polimorphic: :true
+  belongs_to :text, polymorphic: :true
   belongs_to :criterion
 end
