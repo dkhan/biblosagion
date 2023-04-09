a0cdb6a5ceb0de2c613fa61024d8d0b3aa0c74ba
Dennis Khan <khandennis@gmail.com>
2023-04-09 01:44:53 -0400

    Populate ESV translation

--- app/services/dataset_service.rb
+++ app/services/dataset_service.rb
@@ -1,11 +1,28 @@
 module DatasetService
-  # trancate all tables before reset
   def self.populate
     StrongService.populate
     WordService.populate
     BookService.populate
     CriterionService.populate
     ParagraphService.populate
+
+    create_missing_verses
+    
     TranslationService.populate
+  end
+
+  def self.create_missing_verses
+    [
+      { reference: "43_Jhn.005.004", book: "John", chapter: 5,  verse: 4 },
+      { reference: "44_Act.015.034", book: "Acts", chapter: 15, verse: 34 },
+    ].each do |v|
+      book = Book.find_by_name(v[:book])
+      chapter = Chapter.find_by_book_id_and_number(book.id, v[:chapter])
+      Verse.create!(
+        reference: v[:reference],
+        book_id: book.id,
+        chapter_id: chapter.id,
+        number: v[:verse])
+    end
   end
 end
--- app/services/translation_service.rb
+++ app/services/translation_service.rb
@@ -2,28 +2,37 @@ class TranslationService
   require 'rexml/document'
   include REXML
 
-  attr_accessor :xml
-
-  def initialize(filename = "Bible_English_ESV.xml")
-    @xml = File.join(Rails.root, "datasets", filename)
-  end
-
   def self.populate
-    xmlroot.elements.each("BIBLEBOOK") do |book|
-      puts "Book " + book.attributes["bnumber"] + ": " + book.attributes["bname"]
-      # book.elements.each("CHAPTER") do |chapter|
-      #   puts "Chapter: " + chapter.attributes["cnumber"]
-      #   chapter.elements.each("VERS") do |verse|
-      #     puts verse.attributes["vnumber"] + ": " + verse.text
-          
-      #   end
-      # end
-    end
-  end
-
-  def xmlroot
+    xml = File.join(Rails.root, "datasets", "Bible_English_ESV.xml")
     xmlfile = File.new(xml)
     xmldoc = Document.new(xmlfile)
-    xmldoc.root
+    
+    xmldoc.root.elements.each("BIBLEBOOK") do |b|
+      next if b.attributes["bnumber"].to_i < 40
+
+      puts "Book " + b.attributes["bnumber"] + ": " + b.attributes["bname"]
+      book = Book.find_by_number(b.attributes["bnumber"])
+      b.elements.each("CHAPTER") do |c|
+        puts "Chapter: " + c.attributes["cnumber"]
+        chapter = Chapter.find_by_book_id_and_number(book.id, c.attributes["cnumber"])
+        c.elements.each("VERS") do |v|
+          puts v.attributes["vnumber"] + ": " + v.text
+          verse = Verse.find_by_chapter_id_and_number(chapter.id, v.attributes["vnumber"])
+          if verse.present?
+            puts verse.reference
+            Translation.create!(
+              version: "ESV",
+              reference: verse.reference,
+              book_id: book.id,
+              chapter_id: chapter.id,
+              verse_id: verse.id,
+              description: nil,
+              text: v.text)
+          else
+            puts "NO VERSE!!!"
+          end
+        end
+      end
+    end
   end
 end
